e4d28e58d4addf64249524690fb0ace1d95c8cc5
Don-Stevenson <dsteven1@gmail.com>
2021-01-14 11:51:38 -0500

    get .env require mongodb connection working

--- mern-auth-backend/server.js
+++ mern-auth-backend/server.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config;
+require("dotenv").config({path:'/home/don/lighthouse/fung-tech-challenge/.env'});
+// console.log({__dirname})
+const mongoDbConnection = process.env.MONGODB_CONNECTION_STRING;
+// console.log(mongoDbConnection);
 
 //express setup
-const app = express()
-app.use(express.json())
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, ()=> console.log(`the server is listening on ${PORT}`))
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`the server is listening on ${PORT}`));
+
+// mongoose setup
+mongoose.connect(
+  mongoDbConnection,
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+  (err) => {
+    if (err) throw err;
+    else console.log("MongoDB connection is established");
+  }
+);
+
+// routes setup
+app.use("/users", require("./routes/userRouter"));
