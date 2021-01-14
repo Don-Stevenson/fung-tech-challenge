8c13299da1beef963bf1bc75bf8ef6f3f5ce902f
Don-Stevenson <dsteven1@gmail.com>
2021-01-14 18:47:16 -0500

    add conditional buttons for login and register, add logout function, add comments

--- mern-auth-front/src/components/auth/AuthOptions.js
+++ mern-auth-front/src/components/auth/AuthOptions.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useHistory } from "react-router-dom";
+import UserContext from "../../context/UserContext";
 
 export default function AuthOptions() {
+  // geting user data via usecontext with UserContext
+  const { userData, setUserData } = useContext(UserContext);
+
   const history = useHistory();
- // using the usehistory to switch pages on click via the functions below
+  // using the usehistory to switch pages on click via the functions below
   const register = () => {
     history.push("/register");
   };
@@ -11,10 +15,26 @@ export default function AuthOptions() {
   const login = () => {
     history.push("/login");
   };
+  // logout function to reset token, userdata
+  // to undefined and auth token to empty string
+  const logout = () => {
+    setUserData({
+      token: undefined,
+      user: undefined,
+    });
+    localStorage.setItem("auth-token", "")
+  };
   return (
     <nav className="auth-options">
-      <button onClick={register}>Register</button>
-      <button onClick={login}>Login</button>
+      {/* return conditional buttons if logged in or not*/}
+      {userData.user ? (
+        <button onClick={logout}>Logout</button>
+      ) : (
+        <>
+          <button onClick={register}>Register</button>
+          <button onClick={login}>Login</button>
+        </>
+      )}
     </nav>
   );
 }
