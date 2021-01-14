4f74bd3ae93d99440aef8efd0e424a01af9841a5
Don-Stevenson <dsteven1@gmail.com>
2021-01-14 15:12:26 -0500

    import style, add header component

--- mern-auth-front/src/App.js
+++ mern-auth-front/src/App.js
@@ -3,11 +3,15 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import Header from "./components/layout/Header";
+
+import "./style.css"
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
+      <Header/>
         <Switch>
           <Route exact path="/" component={Home} />
         
