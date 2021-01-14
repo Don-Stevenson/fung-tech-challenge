b9a39a458fd80cbc5b07f9cbd05cfccc6f618f1e
Don-Stevenson <dsteven1@gmail.com>
2021-01-14 14:06:06 -0500

    add bassic routes for login and register

--- mern-auth-front/src/App.js
+++ mern-auth-front/src/App.js
@@ -1,16 +1,21 @@
 import React from "react";
-import {BrowserRouter, Switch, Route} from "react-router-dom"
-import Home from './components/pages/Home'
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 
 export default function App() {
-  return <>
-  <BrowserRouter>
-  <Switch>
-    <Route path="/" component={Home}>
-
-    </Route>
-  </Switch>
-  </BrowserRouter>
-  </>
-
+  return (
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        
+          <Route exact path="/login" component={Login} />
+        
+          <Route exact path="/register" component={Register} />
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
 }
