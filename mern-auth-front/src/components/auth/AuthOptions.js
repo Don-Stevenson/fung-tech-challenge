3dba451fb499cb0586e2d99e4700bc40686f3895
Don-Stevenson <dsteven1@gmail.com>
2021-01-14 16:18:57 -0500

    add comment

--- mern-auth-front/src/components/auth/AuthOptions.js
+++ mern-auth-front/src/components/auth/AuthOptions.js
@@ -3,7 +3,7 @@ import { useHistory } from "react-router-dom";
 
 export default function AuthOptions() {
   const history = useHistory();
-
+ // using the usehistory to switch pages on click via the functions below
   const register = () => {
     history.push("/register");
   };
