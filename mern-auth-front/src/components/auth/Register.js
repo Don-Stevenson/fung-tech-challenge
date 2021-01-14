7eccecceec320b5cc6addf4b06d161e6c9628b91
Don-Stevenson <dsteven1@gmail.com>
2021-01-14 18:44:59 -0500

    add logic to create a new user from a form to backend and then login

--- mern-auth-front/src/components/auth/Register.js
+++ mern-auth-front/src/components/auth/Register.js
@@ -1,9 +1,77 @@
-import React from 'react'
+import React, { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import UserContext from "../../context/UserContext";
+import Axios from "axios";
 
 export default function Register() {
-    return (
-        <div>
-            Register
-        </div>
-    )
+
+  // custom useState hooks to set various data
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [passwordCheck, setPasswordCheck] = useState();
+  const [displayName, setDisplayName] = useState();
+
+  const { setUserData } = useContext(UserContext);
+
+  const history = useHistory();
+
+  // function that submits a post new user data to
+  // backend and logins in with that data
+  const submit = async (e) => {
+    e.preventDefault();
+    const newUser = { email, password, passwordCheck, displayName };
+    await Axios.post("http://localhost:5000/users/register", newUser);
+    const loginRes = await Axios.post("http://localhost:5000/users/login", {
+      email,
+      password,
+    });
+
+    // sets the user and token
+    setUserData({
+      token: loginRes.data.token,
+      user: loginRes.data.user,
+    });
+
+    // persists the auth token for refresh and new windows
+    localStorage.setItem("auth-token", loginRes.data.token);
+
+    // using history call to return to homepage
+    history.push("/");
+  };
+
+  // form for registration
+  return (
+    <div className="page">
+      <h2>Register</h2>
+      <form onSubmit={submit}>
+        <label htmlFor="register-email">Email</label>
+        <input
+          id="register-email"
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <label htmlFor="register-password">Password</label>
+        <input
+          id="register-password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="verify your password"
+          onChange={(e) => setPasswordCheck(e.target.value)}
+        />
+
+        <label htmlFor="register-display-name">Display Name</label>
+        <input
+          id="register-display-name"
+          type="text"
+          onChange={(e) => setDisplayName(e.target.value)}
+        />
+
+        <input type="submit" value="Register" />
+      </form>
+    </div>
+  );
 }
