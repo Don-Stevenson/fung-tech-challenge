5f2478450393caaa2a099cecc3def629e0249c87
Don-Stevenson <dsteven1@gmail.com>
2021-01-14 22:11:40 -0500

    add logic to send user to login if not logged in

--- mern-auth-front/src/components/pages/Home.js
+++ mern-auth-front/src/components/pages/Home.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useEffect, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import UserContext from "../../context/UserContext";
 
 export default function Home() {
-    return (
-        <div className="page">
-            You've Reached the Homepage Hooray!
-        </div>
-    )
+  // destructure user data through reducer with UserContext
+  const { userData } = useContext(UserContext);
+  const history = useHistory();
+  // use useFfect to send user to login in no user logged in
+  useEffect(() => {
+    if (!userData.user) history.push("/login");
+  });
+  return <div className="page">You've Reached the Homepage Hooray!</div>;
 }
