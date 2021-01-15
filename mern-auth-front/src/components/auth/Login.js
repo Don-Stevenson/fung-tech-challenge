66d73bf40a6f2bbe5b80b144c54774c02bcf829b
Don-Stevenson <dsteven1@gmail.com>
2021-01-14 22:08:53 -0500

    add logic to display error if present

--- mern-auth-front/src/components/auth/Login.js
+++ mern-auth-front/src/components/auth/Login.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from "react";
 import { useHistory } from "react-router-dom";
 import UserContext from "../../context/UserContext";
+import ErrorNotice from "../misc/ErrorNotice";
 import Axios from "axios";
 
 export default function Login() {
@@ -9,34 +10,45 @@ export default function Login() {
   const [password, setPassword] = useState();
   const { setUserData } = useContext(UserContext);
   const history = useHistory();
+  const [error, setError] = useState();
 
   // function that submits a post new user data to
   // backend and logins in with that data
   const submit = async (e) => {
+    // prevents page refresh on submit
     e.preventDefault();
-    const loginUser = { email, password };
+    try {
+      const loginUser = { email, password };
 
-    const loginRes = await Axios.post(
-      "http://localhost:5000/users/login",
-      loginUser
-    );
+      const loginRes = await Axios.post(
+        "http://localhost:5000/users/login",
+        loginUser
+      );
 
-    // sets the user and token
-    setUserData({
-      token: loginRes.data.token,
-      user: loginRes.data.user,
-    });
+      // sets the user and token
+      setUserData({
+        token: loginRes.data.token,
+        user: loginRes.data.user,
+      });
 
-    // persists the auth token for refresh and new windows in browser
-    localStorage.setItem("auth-token", loginRes.data.token);
+      // persists the auth token for refresh and new windows in browser
+      localStorage.setItem("auth-token", loginRes.data.token);
 
-    // using history call to return to homepage
-    history.push("/");
+      // using history call to return to homepage
+      history.push("/");
+    } catch (error) {
+      // catch the error and set it to state if present
+      error.response.data.msg && setError(error.response.data.msg);
+    }
   };
 
   return (
     <div className="page">
       <h2>Login</h2>
+      {/* if an error display the error message and click it clear it */}
+      {error && (
+        <ErrorNotice message={error} clearError={() => setError(undefined)} />
+      )}
       <form className="form" onSubmit={submit}>
         <label htmlFor="login-email">Email</label>
         <input
